76c57b582d362f6297180d399e28570247bbbb95
heng guo <heng.guo@ist.osaka-u.ac.jp>
2021-08-04 22:36:34 +0900

    It looks long iteration works for depth discontinuity

--- bash/bash_uniform_23.py
+++ bash/bash_uniform_23.py
@@ -4,16 +4,15 @@ import numpy as np
 
 
 para_folder = '/mnt/workspace2020/heng/project/data/supp'
-shape_set = np.genfromtxt(os.path.join(para_folder, 'shape_list.csv'), delimiter=',', dtype=np.str)
+shape_set = np.genfromtxt(os.path.join(para_folder, 'shape_list_test.csv'), delimiter=',', dtype=np.str)
 scale_set = np.genfromtxt(os.path.join(para_folder, 'scale_list_64_64.csv'), delimiter=',', dtype=np.int)
 base_data_dir = '/mnt/workspace2020/heng/project/data/output_dir_near_light'
 gpu_id = 4
 
 scale_set = [[256, 256]]
-index = np.array([])
 commit_id = '0fb0f354'
 for resolution in scale_set:
-    for shape_name in shape_set[5:]:
+    for shape_name in shape_set:
         filename_str = 'orthographic/lambertian/scale_{rx}_{ry}/wo_castshadow/shading/'.format(
             rx=int(resolution[0]), ry=int(resolution[1]))
         data_dir = os.path.join(base_data_dir, shape_name.split('.')[0], filename_str)
