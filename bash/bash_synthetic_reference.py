b5281235cb39e882b2941b71bf7059f014ddf652
heng guo <heng.guo@ist.osaka-u.ac.jp>
2021-08-08 22:49:13 +0900

    The loss of the sphere is 0.000003 if given the depth initialization as GT, So we believe the version of QY18. The result is very sensitive to the depth initialiaiton

--- bash/bash_synthetic_reference.py
+++ bash/bash_synthetic_reference.py
@@ -7,12 +7,12 @@ from Reference.QY18 import run_QW18
 para_folder = '../data/supp'
 shape_set = np.genfromtxt(os.path.join(para_folder, 'shape_list.csv'), delimiter=',', dtype=np.str)
 scale_set = np.genfromtxt(os.path.join(para_folder, 'scale_list_64_64.csv'), delimiter=',', dtype=np.int)
-base_data_dir = '../data/output_dir_near_light'
+base_data_dir = r'F:\Project\SIREN\siren\data\output_dir_near_light'
 
-scale_set = [[256, 256], [128, 128]]
-commit_id = 'ef7f97f8'
+scale_set = [[512, 512]]
+commit_id = '8c3bb149'
 for resolution in scale_set:
-    for shape_name in shape_set[9:]:
+    for shape_name in shape_set:
         filename_str = 'perspective/lambertian/scale_{rx}_{ry}/wo_castshadow/shading/'.format(
             rx=int(resolution[0]), ry=int(resolution[1]))
         data_dir = os.path.join(base_data_dir, shape_name.split('.')[0], filename_str)
