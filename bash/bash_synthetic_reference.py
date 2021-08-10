2850150313312707f7d28c0c529196268a252d70
heng guo <heng.guo@ist.osaka-u.ac.jp>
2021-08-10 15:50:40 +0900

    Test the sensitivity of the depth initilization

--- bash/bash_synthetic_reference.py
+++ bash/bash_synthetic_reference.py
@@ -9,7 +9,7 @@ shape_set = np.genfromtxt(os.path.join(para_folder, 'shape_list.csv'), delimiter
 scale_set = np.genfromtxt(os.path.join(para_folder, 'scale_list_64_64.csv'), delimiter=',', dtype=np.int)
 base_data_dir = r'F:\Project\SIREN\siren\data\output_dir_near_light'
 
-commit_id = '4797172e'
+commit_id = 'cc2d0382'
 for resolution in scale_set:
     for shape_name in shape_set:
         filename_str = 'perspective/lambertian/scale_{rx}_{ry}/wo_castshadow/shading/'.format(
