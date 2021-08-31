a94725b5374a3e6867f28580a110939eb3370690
Heng <heng.guo@ist.osaka-u.ac.jp>
2021-08-31 20:33:46 +0900

    debug real data with LUCES

--- data/real_data/process_real_data_LUCES.py
+++ data/real_data/process_real_data_LUCES.py
@@ -34,6 +34,24 @@ depth_gt = read_gt_depth(para_folder, scale)
 depth_gt = cv2.resize(depth_gt, None, fx=scale, fy=scale, interpolation=cv2.INTER_NEAREST)
 depth_gt = depth_gt * (1e-3)
 
+
+row, column = mask.shape
+yy, xx = np.mgrid[:row, :column]
+yy = np.flip(yy, axis=0)
+xx = np.flip(xx, axis=1)
+pixel_coords = np.stack([xx, yy], axis=-1).astype(np.float32)  ## -yy, xx
+pixel_coords[:, :, 0] = pixel_coords[:, :, 0] / (row - 1)  # xx
+pixel_coords[:, :, 1] = pixel_coords[:, :, 1] / (column - 1)
+pixel_coords -= 0.5
+pixel_coords *= 2.
+
+focal_len = 8 #mm
+sensor_w = focal_len * ncols / f *1e-3
+sensor_h = focal_len * nrows / f *1e-3
+pixel_coords[:, :, 0] = pixel_coords[:, :, 0] * (sensor_w / 2) * depth_gt / focal_len
+pixel_coords[:, :, 1] = pixel_coords[:, :, 1] * (sensor_h / 2) * depth_gt / focal_len
+point_set_world = np.concatenate([pixel_coords, depth_gt[:, :, np.newaxis]], axis=2)
+
 imgs = load_images(para_folder, nrows, ncols, numLight)
 img_set = imgs.transpose([3, 0, 1, 2])
 
@@ -48,6 +66,11 @@ np.save(os.path.join(save_dir, 'render_para/camera_K.npy'), camera_K)
 np.save(os.path.join(save_dir, 'render_para/normal_obj.npy'), N_gt_obj)
 np.save(os.path.join(save_dir, 'render_para/normal_world.npy'), N_gt_world)
 np.save(os.path.join(save_dir, 'render_para/depth.npy'), depth_gt)
+np.save(os.path.join(save_dir, 'point_set_world.npy'), point_set_world)
+
+from hutils.draw_3D import generate_mesh
+point_3d_name = os.path.join(save_dir, 'shape.png')
+generate_mesh(point_set_world, mask, point_3d_name, window_size = (1920, 1080), title='shape')
 
 
 run_save_config = True
@@ -59,7 +82,7 @@ if run_save_config:
     camera_K_crop_raw = np.load(os.path.join(save_dir, 'render_para/camera_K.npy'))
     crop_rec_center = [ncols / 2, nrows / 2]
     configinfo = {
-            "focal_len": "8", "fx": "{}".format(camera_K_crop_raw[0, 0]), "fy": "{}".format(camera_K_crop_raw[1, 1]),
+            "focal_len": "{}".format(focal_len), "fx": "{}".format(camera_K_crop_raw[0, 0]), "fy": "{}".format(camera_K_crop_raw[1, 1]),
             "cx": "{}".format(crop_rec_center[0] - camera_K_crop_raw[0, 2]),
             "cy": "{}".format(crop_rec_center[1] - camera_K_crop_raw[1, 2]),
             "img_h": "{}".format(img_set.shape[1]), "img_w": "{}".format(img_set.shape[2]),
